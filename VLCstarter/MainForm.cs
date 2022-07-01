305e27996ec350e45b0c4f92a1bc2725ca411b28
Victor <publicdomainvic@gmail.com>
2022-07-01 11:43:09 -0400

    Add OnExitToolStripMenuItemClick()

--- VLCstarter/MainForm.cs
+++ VLCstarter/MainForm.cs
@@ -259,7 +259,8 @@ namespace VLCstarter
         /// <param name="e">Event arguments.</param>
         private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Close program
+            this.Close();
         }
     }
 }
